369fa2c1805a88f34959fdd5a67a2fe10dd39199
Yaseen Quraishi <yaseen.quraishi@digital.hmrc.gov.uk>
2018-03-09 14:39:16 +0000

    db

--- helloapp/helloapp/models.py
+++ helloapp/helloapp/models.py
@@ -7,3 +7,16 @@ class Artist(models.Model):
     ContactNumber = models.CharField(max_length=12)
     ContactEmail = models.EmailField(max_length=20)
     Country = models.CharField(max_length=20)
+
+class Customers(models.Model):
+    id = models.IntegerField(max_length=20), 'primary_key=True'
+    FirstName = models.CharField(max_length=20)
+    LastName = models.CharField(max_length=20)
+    Title = models.CharField(max_length=5)
+    PhoneNumber = models.CharField(max_length=20)
+    StreetAddress = models.CharField(max_length=50)
+    PostalCode = models.CharField(max_length=50)
+    State = models.CharField(max_length=20)
+    Country = models.CharField(max_length=20)
+    CountryCode = models.CharField(max_length=20)
+    TicketsBought = models.CharField(max_length=20)
